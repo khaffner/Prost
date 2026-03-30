357dd131fad048bdbff288dae1441db42e447227
Kevin Haffner <khaffner@gmail.com>
2026-03-30 21:24:39 +0200

    fix: Replace non-working erroractions with trycatch blocks

--- prost-input/payloads/nodeinfo.ps1
+++ prost-input/payloads/nodeinfo.ps1
@@ -1,12 +1,50 @@
 if (!(Get-InstalledModule -Name linuxinfo -RequiredVersion 0.0.8 -ErrorAction SilentlyContinue)) {
     Install-Module -Name linuxinfo -Force -RequiredVersion 0.0.8
 }
+Import-Module -Name linuxinfo -RequiredVersion 0.0.8 -Force
 
 $info = @{}
-$info.Battery = Get-BatteryInfo
-$info.Computer = Get-ComputerInfo
-$info.Display = Get-DisplayInfo
-$info.Network = Get-NetworkInfo -IncludePublicIP
-$info.OS = Get-OSInfo
-$info.SystemUptime = Get-SystemUptime
+
+try {
+    $info.Battery = Get-BatteryInfo
+}
+catch {
+
+}
+
+try {
+    $info.Computer = Get-ComputerInfo
+}
+catch {
+
+}
+
+try {
+    $info.Display = Get-DisplayInfo
+}
+catch {
+
+}
+
+try {
+    $info.Network = Get-NetworkInfo -IncludePublicIP
+}
+catch {
+
+}
+
+try {
+    $info.OS = Get-OSInfo
+}
+catch {
+
+}
+
+try {
+    $info.SystemUptime = Get-SystemUptime
+}
+catch {
+
+}
+
 $info | ConvertTo-Json -Depth 10 | Out-File -FilePath "$global:OutputFolder/$global:ID-nodeinfo.json" -Encoding UTF8 -Force
