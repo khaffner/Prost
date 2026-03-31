54e9fcd376ad9f9a04375627e75baf8cf362344e
Kevin Haffner <khaffner@gmail.com>
2026-03-31 20:37:05 +0200

    feat: Add memoryusedpercent

--- prost-input/payloads/nodeinfo.ps1
+++ prost-input/payloads/nodeinfo.ps1
@@ -12,6 +12,7 @@ $info.Network = Get-NetworkInfo -IncludePublicIP
 $info.OS = Get-OSInfo
 $info.UptimeDays = [int](Get-Uptime).TotalDays
 $info.LoadPercent = [int]([float](Get-Content /proc/loadavg).Split(' ')[1] / [int](nproc) * 100)
+$info.MemoryUsedPercent = & { $m = (free | sls Mem).Line -split '\s+'; [int]($m[2] / $m[1] * 100) }
 $info.AptUpdates = apt list --upgradable 2>/dev/null | Select-Object -Skip 1
 $info.PSVersion = $PSVersionTable.PSVersion.ToString()
 $info.TimeStamp = Get-Date -Format "o"
