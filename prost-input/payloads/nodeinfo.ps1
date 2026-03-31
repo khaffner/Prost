d1dc6f250007e89575e26d8f716b4fa2cf07bc90
Kevin Haffner <khaffner@gmail.com>
2026-03-31 23:11:39 +0200

    fix: Expand aliases

--- prost-input/payloads/nodeinfo.ps1
+++ prost-input/payloads/nodeinfo.ps1
@@ -13,7 +13,7 @@ $info.Network.PublicIP = $info.Network.PublicIP.Trim() # Remove annoying whitesp
 $info.OS = try { Get-OSInfo } catch { $null }
 $info.UptimeDays = try { [int](Get-Uptime).TotalDays } catch { $null }
 $info.LoadPercent = try { [int]([float](Get-Content /proc/loadavg).Split(' ')[1] / [int](nproc) * 100) } catch { $null }
-$info.MemoryUsedPercent = try { $m = (free | sls Mem).Line -split '\s+'; [int]($m[2] / $m[1] * 100) } catch { $null }
+$info.MemoryUsedPercent = try { $m = (free | Select-String Mem).Line -split '\s+'; [int]($m[2] / $m[1] * 100) } catch { $null }
 $info.AptUpdates = try { apt list --upgradable 2>/dev/null | Select-Object -Skip 1 } catch { $null }
 $info.PSVersion = $PSVersionTable.PSVersion.ToString()
 $info.TimeStamp = Get-Date -Format "o"
