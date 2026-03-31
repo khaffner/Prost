f14b902dac2547039ef6409b783a8ac30cf05ac7
Kevin Haffner <khaffner@gmail.com>
2026-03-31 21:07:42 +0200

    style: Inline trycatch instead of erroractionpreference

--- prost-input/payloads/nodeinfo.ps1
+++ prost-input/payloads/nodeinfo.ps1
@@ -3,18 +3,15 @@ if (!(Get-InstalledModule -Name linuxinfo -RequiredVersion 0.0.8 -ErrorAction Si
 }
 Import-Module -Name linuxinfo -RequiredVersion 0.0.8 -Force
 
-$ErrorActionPreference = "SilentlyContinue"
 $info = @{}
-$info.Battery = Get-BatteryInfo
-$info.Computer = Get-ComputerInfo
-$info.Display = Get-DisplayInfo
-$info.Network = Get-NetworkInfo -IncludePublicIP
-$info.OS = Get-OSInfo
-$info.UptimeDays = [int](Get-Uptime).TotalDays
-$info.LoadPercent = [int]([float](Get-Content /proc/loadavg).Split(' ')[1] / [int](nproc) * 100)
-$info.MemoryUsedPercent = & { $m = (free | sls Mem).Line -split '\s+'; [int]($m[2] / $m[1] * 100) }
-$info.AptUpdates = apt list --upgradable 2>/dev/null | Select-Object -Skip 1
+$info.Battery = try { Get-BatteryInfo } catch { $null }
+$info.Computer = try { Get-ComputerInfo } catch { $null }
+$info.Network = try { Get-NetworkInfo -IncludePublicIP } catch { $null }
+$info.OS = try { Get-OSInfo } catch { $null }
+$info.UptimeDays = try { [int](Get-Uptime).TotalDays } catch { $null }
+$info.LoadPercent = try { [int]([float](Get-Content /proc/loadavg).Split(' ')[1] / [int](nproc) * 100) } catch { $null }
+$info.MemoryUsedPercent = try { $m = (free | sls Mem).Line -split '\s+'; [int]($m[2] / $m[1] * 100) } catch { $null }
+$info.AptUpdates = try { apt list --upgradable 2>/dev/null | Select-Object -Skip 1 } catch { $null }
 $info.PSVersion = $PSVersionTable.PSVersion.ToString()
 $info.TimeStamp = Get-Date -Format "o"
-$ErrorActionPreference = "Stop"
 $info | ConvertTo-Json -Depth 10 | Out-File -FilePath "$global:OutputFolder/$global:ID-nodeinfo.json" -Encoding UTF8 -Force
